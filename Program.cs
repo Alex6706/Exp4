eefbd37810531dce829d5d4d565b1549e3a331c7
Alex6706 <kvslanz@gmail.com>
2023-01-29 20:00:59 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое число ");
+double N = Convert.ToInt32(Console.ReadLine());
+double M = Convert.ToInt32(0);
+
+while (M <= N)
+    {
+        if (M % 2 == 0)
+            {
+                Console.Write($" {M},");
+                M = M + 1;
+            }
+        else
+            {
+                M = M + 1;
+            }
+
+    }
